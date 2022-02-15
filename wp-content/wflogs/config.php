a4932293299fa379ab291a834dd1c5517b4ea917
docans <docansey@gmail.com>
2022-02-15 14:34:23 -0500

    base zero

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1645463985;s:7:"authKey";s:64:"8(G.Zk4.T-F>%B5w1A|,9&0bzBn4.oI#:d5]!/R*:LF],uPP!N0Ey!9w%0rhrk!I";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:4064;}
